27da499b80824d7fa0d6bdb178379825376ee8f0
Ahmad Kaheel <a.kaheel@qu.edu.sa>
2024-02-08 10:32:06 +0300

    Add Cleaning a specific field attribute

--- QUurls/urlShort/forms.py
+++ QUurls/urlShort/forms.py
@@ -1,20 +1,27 @@
 from django import forms
 from .models import ShortLink
-import random
-import string
+from django.core.exceptions import ValidationError
 
 class ShortLinkForm(forms.ModelForm):
     class Meta:
         model = ShortLink
         fields = ['original_url','short_code']
 
+
+class ContactForm(forms.Form):
+    def clean_recipients(self):
+        short_code = self.cleaned_data["short_code"]
+        if not short_code:
+            raise ValidationError("Empty original_url")
+        return short_code
+
 class ShortLinkFormMixin():
-    def generate_short_code(self, number_of_characters=4):
-        while True:
-            short_code = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(number_of_characters))
-            if not ShortLink.objects.filter(short_code=short_code).exists():
-                return short_code
-                    
+    # def generate_short_code(self, number_of_characters=4):
+    #     while True:
+    #         short_code = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(number_of_characters))
+    #         if not ShortLink.objects.filter(short_code=short_code).exists():
+    #             return short_code
+    
     def form_valid(self, form):
         self.model.original_url = form.cleaned_data['original_url']
         short_code = self.generate_short_code()
--- QUurls/urlShort/models.py
+++ QUurls/urlShort/models.py
@@ -1,5 +1,7 @@
 from django.db import models 
 from django.utils.text import slugify
+import random
+import string
 
 # Create your models here.
 
@@ -8,6 +10,15 @@ class ShortLink(models.Model):
     short_code = models.CharField(max_length=4, unique=True)
     slug = models.SlugField(unique=True, null=True, blank=True)
     
+    def clean_original_url(self):
+        return 
+    
+    def clean_short_code(self,number_of_characters=4):
+        while True:
+            short_code = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(number_of_characters))
+            if not ShortLink.objects.filter(short_code=short_code).exists():
+                return short_code
+            
     def save(self, *args, **kwargs):
         self.slug = slugify(self.short_code)
         super().save(*args, **kwargs)
